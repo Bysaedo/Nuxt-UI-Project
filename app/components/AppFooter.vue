74f68746a919d3fee381bb7f1fbc152dfe4c05da
Bysaedo <by.saedo@gmail.com>
2026-02-07 00:06:01 -0600

    Setup and layout completed

---
+++ app/components/AppFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <UFooter
+    ><p>Copyright © {{ new Date().getFullYear() }}</p>
+    <UNavigationMenu :items="items" variant="link"
+  /></UFooter>
+</template>
+
+<script setup lang="ts">
+import type { NavigationMenuItem } from "@nuxt/ui";
+
+const items: NavigationMenuItem[] = [
+  {
+    label: "GitHub",
+    to: "https://github.com/Bysaedo",
+    target: "_blank",
+    icon: "i-simple-icons-github",
+  },
+  {
+    label: "LinkedIn",
+    to: "https://linkedin.com/in/sebastian-aedo-vidal",
+    target: "_blank",
+    icon: "i-simple-icons-linkedin",
+  },
+];
+</script>
---
+++ app/components/AppHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <UHeader title="SATM">
+    <template #title>
+      <div class="flex items-center gap-2">
+        <UIcon name="i-lucide-flame" class="size-5" />
+        <span>SATM</span>
+      </div>
+    </template>
+
+    <UNavigationMenu :items="items" />
+
+    <template #right>
+      <UColorModeButton />
+    </template>
+  </UHeader>
+</template>
+
+<script setup lang="ts">
+import type { NavigationMenuItem } from "@nuxt/ui";
+
+const items = ref<NavigationMenuItem[]>([
+  {
+    label: "Home",
+    icon: "i-lucide-house",
+    to: "/",
+  },
+  {
+    label: "Search tasks",
+    icon: "i-lucide-search",
+    to: "/searchPage",
+  },
+]);
+</script>
